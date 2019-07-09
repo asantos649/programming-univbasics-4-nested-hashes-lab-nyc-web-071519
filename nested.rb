2638d69fddbbf77d08b550e318bda2a0704dcbe1
Andrew Santos <asantos@alumni.nd.edu>
2019-07-09 21:32:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,10 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp","C"]
+#    :languages => ["LISP", "C"]]
+programmer_hash
 end
 
 def changing_alan
